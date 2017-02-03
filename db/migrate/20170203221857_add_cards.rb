d6fac8fae5671bcbfeff461318431ff5abda913f
Lisa Holter <lkholter@gmail.com>
2017-02-03 17:34:48 -0500

    cards, decks

--- db/migrate/20170203221857_add_cards.rb
+++ db/migrate/20170203221857_add_cards.rb
@@ -1,6 +1,6 @@
-class AddCocktails < ActiveRecord::Migration[5.0]
+class AddCards < ActiveRecord::Migration[5.0]
   def change
-    create_table :cocktails do |t|
+    create_table :cards do |t|
       t.string :name
       t.string :recipe
       t.string :cocktail_type
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170203221104) do
+ActiveRecord::Schema.define(version: 20170203223238) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "cards", force: :cascade do |t|
+    t.string   "name"
+    t.string   "recipe"
+    t.string   "cocktail_type"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+  end
+
+  create_table "decks", force: :cascade do |t|
+    t.string   "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "first_name"
