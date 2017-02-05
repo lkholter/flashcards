4ae9534265b80c9f534b810fb4177309dd0f51e9
Lisa Holter <lkholter@gmail.com>
2017-02-04 22:20:55 -0500

    cards management

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,5 +1,28 @@
 class CardsController < ApplicationController
+  before_action :find_record, except: [:index, :create]
+
   def index
     @cards = Card.all
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  private
+
+  def find_record
+    @card = Card.find params[:id] if params[:id]
   end
 end
