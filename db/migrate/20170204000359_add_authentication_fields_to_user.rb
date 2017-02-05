a960698626add03f8049465731e08c6236d556bb
Lisa Holter <lkholter@gmail.com>
2017-02-04 19:46:26 -0500

    drop auth fields

--- db/migrate/20170204000359_add_authentication_fields_to_user.rb
+++
@@ -1,7 +0,0 @@
-class AddAuthenticationFieldsToUser < ActiveRecord::Migration[5.0]
-  def change
-    add_column :users, :email, :string
-    add_column :users, :password, :string
-    add_column :users, :password_confirmation, :string
-  end
-end
